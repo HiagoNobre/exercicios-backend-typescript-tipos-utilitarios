c29efe4046983e90b522cfa397b21ae4aae38d3a
nobreh <nobrehiago21@hotmail.com>
2023-05-22 20:00:49 -0300

    retorno da funcao somente leitura

--- 03/scripts.ts
+++ 03/scripts.ts
@@ -6,8 +6,22 @@ type Conn = {
     dbname: string
 }
 
-const conexao = (dados: Conn) => {
+const dados = {
+    username: "dslf",
+    password: "sdklfjs",
+    host: "lasfdk",
+    port: "lsdj",
+    dbname: "lsdflkd"
+}
+
+type Conexao = {
+    driver: string,
+    url: string
+}
+
+const conexao = (dados: Conn): Readonly<Conexao> => {
     const { username, password, host, port, dbname } = dados;
+
     return {
         driver: 'postgres',
         url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`
